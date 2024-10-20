a450b7b67dce1b5a760e078e87b2cf6d2cc365a6
Account <myaccount@Accounts-MacBook-Pro.local>
2024-10-20 19:43:18 +0200

    changing app.py

--- app.py
+++ app.py
@@ -98,51 +98,79 @@ with st.sidebar:
 st.title('\U0001f3c3 Półmaraton Estymator')
 
 st.write('---')
-with st.popover('Przedstaw się', use_container_width=True):
-    name = st.text_input('Witaj! Jak masz na imię?')
-st.markdown(f'## Cześć {name}! :hand:')
-st.markdown('###### Znajdujesz się w aplikacji imitującej estymator półmaratonu biegowego.\n###### Uzupełnij poniższe informacje, a dzięki modelowi AI - dowiesz się ile wyniósłby Twój czas ukończenia półmaratonu.\n###### :arrow_left: Z lewej strony możesz wybrać model AI.')
 
-with st.expander('Opisz siebie', expanded=False):
-    st.markdown('#### Podaj następujące informacje o sobie:\n - wiek\n - płeć\n - czas na 5km\n - czas na 10km\n - tempo w jakim jesteś w stanie przebiec 15km\n ###### <span style="color:red">Przykład:</span> Mam 28 lat, jestem mężczyzną, 5km przebiegam w 22 minuty, 10km w 50 minut, natomiast na 15km utrzymam tempo 5:30 min/km.', unsafe_allow_html=True)
-    user_data = st.text_area('Tutaj wpisz informacje:')
+if not 'name' in st.session_state:
+    st.session_state['name'] = ""
 
-if user_data:
+if not 'greeted' in st.session_state:
+    st.session_state['greeted'] = False
 
-    data_for_predict = retrieve_structure_observed(user_data, User)
+if not st.session_state['greeted']:
+    name = st.text_input('Witaj! Jak masz na imię?', '')
+    if name:
+        st.session_state['name'] = name
+        st.session_state['greeted'] = True
+        st.rerun()
 
-    missing_columns = []
+if st.session_state['greeted']:
+    st.markdown(f'## Cześć {st.session_state["name"]}! :hand:')
+    st.markdown('###### Znajdujesz się w aplikacji imitującej estymator półmaratonu biegowego.\n###### Uzupełnij poniższe informacje, a dzięki modelowi AI - dowiesz się ile wyniósłby Twój czas ukończenia półmaratonu.\n###### :arrow_left: Z lewej strony możesz wybrać model AI.')
 
-    if not data_for_predict['age']:
-        missing_columns.append('wiek')
-    if not data_for_predict['gender']:
-        missing_columns.append('płeć')
-    if data_for_predict['total_seconds_5km']==0:
-        missing_columns.append('czas na 5km')
-    if data_for_predict['total_seconds_10km']==0:
-        missing_columns.append('czas na 10km')
-    if not data_for_predict['pace_15km']:
-        missing_columns.append('tempo na 15km')
+    with st.expander('Opisz siebie', expanded=False):
+        st.markdown('#### Podaj następujące informacje o sobie:\n - wiek\n - płeć\n - czas na 5km\n - czas na 10km\n - tempo w jakim jesteś w stanie przebiec 15km\n ###### <span style="color:red">Przykład:</span> Mam 28 lat, jestem mężczyzną, 5km przebiegam w 22 minuty, 10km w 50 minut, natomiast na 15km utrzymam tempo 5:30 min/km.', unsafe_allow_html=True)
+        
+        if not 'user_data' in st.session_state:
+            st.session_state['user_data'] = ""
+        
+        user_data = st.text_area('Tutaj wpisz informacje:', value=st.session_state['user_data'])
+        
+        if st.button('Estymuj czas przebiegnięcia półmaratonu', use_container_width=True):
+        
+            if user_data:
+                st.session_state['user_data'] = user_data
 
-    if missing_columns:
-        st.error(f'Brakuje informacji: {", ".join(missing_columns)}. Proszę je uzupełnić!')    
-        #st.write(data_for_predict)
-    else:
-        user_df = pd.DataFrame({
-            'Wiek' : data_for_predict['age'],
-            'Płeć' : 'M' if 'male' in data_for_predict['gender'].lower() or 'mężczyzna' in data_for_predict['gender'].lower() else 'K',
-            '5km Czas [sek]' : data_for_predict['total_seconds_5km'],
-            '10km Czas [sek]' : data_for_predict['total_seconds_10km'],
-            '15km Tempo [min/km]' : data_for_predict['pace_15km'],
-        }, index=[0])
+                data_for_predict = retrieve_structure_observed(st.session_state['user_data'], User)
 
-        # predykcja na podstawie danych użytkownika
-        model = get_model()
-        prediction = predict_model(model, data=user_df)
-        # wynik predykcji w sekundach
-        prediction_seconds = round(prediction["prediction_label"][0], 2)
-        # wynik predykcji w formacie czasowym: H:M:S
-        prediction_time = str(datetime.timedelta(seconds=int(prediction_seconds)))
-        st.success(f'Estymowany czas ukończenia półmaratonu (w formacie: H:M:S) wynosi {prediction_time}')
-        #st.write(data_for_predict)
+                missing_columns = []
+
+                if not data_for_predict['age']:
+                    missing_columns.append('wiek')
+                if not data_for_predict['gender']:
+                    missing_columns.append('płeć')
+                if data_for_predict['total_seconds_5km']==0:
+                    missing_columns.append('czas na 5km')
+                if data_for_predict['total_seconds_10km']==0:
+                    missing_columns.append('czas na 10km')
+                if not data_for_predict['pace_15km']:
+                    missing_columns.append('tempo na 15km')
+
+                if missing_columns:
+                    st.error(f'Brakuje informacji: {", ".join(missing_columns)}. Proszę je uzupełnić!')    
+                    #st.write(data_for_predict)
+                else:
+                        user_df = pd.DataFrame({
+                            'Wiek' : data_for_predict['age'],
+                            'Płeć' : 'M' if 'male' in data_for_predict['gender'].lower() or 'mężczyzna' in data_for_predict['gender'].lower() else 'K',
+                            '5km Czas [sek]' : data_for_predict['total_seconds_5km'],
+                            '10km Czas [sek]' : data_for_predict['total_seconds_10km'],
+                            '15km Tempo [min/km]' : data_for_predict['pace_15km'],
+                        }, index=[0])
+
+                        # predykcja na podstawie danych użytkownika
+                        model = get_model()
+                        prediction = predict_model(model, data=user_df)
+                        # wynik predykcji w sekundach
+                        prediction_seconds = round(prediction["prediction_label"][0], 2)
+                        # wynik predykcji w formacie czasowym: H:M:S
+                        prediction_time = str(datetime.timedelta(seconds=int(prediction_seconds)))
+                        estimate_result = st.success(f'Estymowany czas ukończenia półmaratonu (w formacie: H:M:S) wynosi {prediction_time}')
+                        #st.write(data_for_predict)
+                        if estimate_result:
+                            st.session_state['user_data'] = ""
+                            if st.button('Wyczyść'):
+                                st.rerun()
+
+            else:
+                st.error('Najpierw dodaj informacje o sobie!')
+        
 
